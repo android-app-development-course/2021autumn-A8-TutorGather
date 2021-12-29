13be21fb4f4b815c494cdaef10cce1e650b159aa
TianTianSuper <576221590@qq.com>
2021-12-29 13:14:53 +0800

    Update Profile UI

--- TutorGather/app/src/main/java/com/example/mobile_phone/fragment/PersonalFragment.kt
+++ TutorGather/app/src/main/java/com/example/mobile_phone/fragment/PersonalFragment.kt
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.example.mobile_phone.R
 import com.example.mobile_phone.bean.User
 import kotlinx.android.synthetic.main.fragment_personal.*
@@ -27,8 +29,41 @@ class PersonalFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_personal, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        val profile_img :ImageView = profile_image
+//        profile_img.setImageResource(R.drawable.default_profile_image_128)
+//
+////        val options: BitmapFactory.Options = BitmapFactory.Options()
+////        options.inDither = false
+////        options.inPurgeable = true
+////        options.inInputShareable = true
+////        options.inTempStorage = ByteArray(1024 * 32)
+////
+////        val bm: Bitmap =
+////            BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length, options)
+////        alert_photo.setImageBitmap(bm)
+//        val user_name : TextView = name_content
+//        user_name.text = User.name
+//
+//        val identification : TextView = id_content
+//        if (User.canBeTeacher){
+//            identification.text = "老师"
+//        }
+//        else{
+//            identification.text = "家长"
+//        }
+//        val address :TextView = address_content
+//        address.text = User.address
+//
+//        val btn_at_help : Button = btn_help
+//        btn_at_help.setOnClickListener {
+//            Toast.makeText(this, "SUCC", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val profile_img :ImageView = profile_image
         profile_img.setImageResource(R.drawable.default_profile_image_128)
 
@@ -53,6 +88,7 @@ class PersonalFragment : Fragment() {
         }
         val address :TextView = address_content
         address.text = User.address
+
     }
 
 }
